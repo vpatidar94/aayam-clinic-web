63480e29b4c4a33199995ae5c30f0dab14143b98
Vinay Patidar <vpatidar94@gmail.com>
2023-06-22 01:14:33 +0530

    change

--- main.js
+++ main.js
@@ -38,12 +38,12 @@ app.use((req, res, next) => {
 // this.app.get('/*', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../web-app/build/index.html'));
 // });
-// app.use(express.static('build'));
-app.use(express.static(path.join(__dirname, './aayamskinclinic/build')));
+app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, './aayamskinclinic/build')));
 
 app.get('/*', (req, res) => {
-    // res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    res.sendFile(path.join(__dirname, './aayamskinclinic/build/index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    // res.sendFile(path.join(__dirname, './aayamskinclinic/build/index.html'));
 });
 // }
 
